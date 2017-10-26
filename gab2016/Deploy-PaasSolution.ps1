d4290bacfa11f3e60d3c9880e6bbcb46c534843b
James Bannan <james@bannan.com.au>
2017-10-26 12:33:02 -0700

    Added artifacts from GAB 2016

---
+++ gab2016/Deploy-PaasSolution.ps1
@@ -0,0 +1,32 @@
+﻿### Define variables
+$location = 'Australia Southeast'
+$resourceGroupName = 'gab2016-arm-paas'
+$resourceDeploymentName = 'gab2016-arm-paas-deployment'
+$templatePath = $env:USERPROFILE + '\Documents\git\presentations\gab2016'
+$templateFile = 'paasDeploy_v2.json'
+$template = $templatePath + '\' + $templateFile
+
+$locations = @( `
+    'East US', `
+    'West Europe', `
+    'Southeast Asia', `
+    'Australia Southeast'
+    )
+
+### Define additional template parameters
+$additionalParameters = New-Object -TypeName Hashtable
+$additionalParameters['webAppLocation'] = $locations
+
+### Create Resource Group
+New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $Location `
+    -Verbose -Force
+
+### Deploy Resources
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceDeploymentName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    @additionalParameters `
+    -Verbose -Force
